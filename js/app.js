f82e40bd25598540a181211fab4b227fb08def19
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-07-14 13:54:52 -0700

    Fixed the nav

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 window.addEventListener( 'scroll', function( event ) {
   var navBar = document.getElementsByTagName( 'nav' )[ 0 ];
-  var right1 = document.getElementsByTagName( 'money' )[ 0 ];
-  var right2 = document.getElementsByTagName( 'support' )[ 0 ];
-  var left1 = document.getElementsByTagName( 'satisfaction' )[ 0 ];
-  var left2 = document.getElementsByTagName( 'scheduling' )[ 0 ];
+  // var right1 = document.getElementsByTagName( 'money' )[ 0 ];
+  // var right2 = document.getElementsByTagName( 'support' )[ 0 ];
+  // var left1 = document.getElementsByTagName( 'satisfaction' )[ 0 ];
+  // var left2 = document.getElementsByTagName( 'scheduling' )[ 0 ];
 
   if( window.pageYOffset > 0 ) {
     navBar.classList.add( 'overlay' );
-    right2.style.right = 50px;
+    // .style.right = 50 + 'px';
   } else {
     navBar.classList.remove( 'overlay' );
   }
-  console.log(window.pageYOffset);
-  if( window.pageYOffset > 700) {
-    right2.style.right = 300px;
-  }
+  // console.log(window.pageYOffset);
+  // if( window.pageYOffset > 700) {
+  //   right2.style.right = 300px;
+  // }
 });
 
 // function myMove() {
