8393b9733469d90253af167b9c5d9beebd49ebb3
Majid Rahimi <Majid88Rahimi@gmail.com>
2016-07-15 09:05:31 -0700

    added animation to home page

--- js/app.js
+++ js/app.js
@@ -39,3 +39,54 @@ function zoom_in( yOffset ) {
     zoomableDivs().forEach( applyZoomIn );
   }
 }
+
+
+var thingsToType = [
+  ' ONSITE COMPUTER SUPPORT'
+]
+
+var domNode = document.querySelector('.tagline');
+var pauseDelay = 500;
+
+var thingToTypeIndex = 0;
+var currentValue = '';
+var deleting = false;
+
+function update(){
+  var thingToType = thingsToType[thingToTypeIndex];
+  if (!deleting && currentValue.length === thingToType.length){
+    deleting = true;
+    setTimeout(update, pauseDelay);
+    return;
+  }
+  if (deleting && currentValue === ""){
+    deleting = false;
+    thingToTypeIndex++
+    if (thingToTypeIndex >= thingsToType.length){
+      thingToTypeIndex = 0;
+    }
+    setTimeout(update, pauseDelay);
+    return;
+  }
+  if (deleting){
+    currentValue = currentValue.slice(0,-1)
+    setTimeout(update, getRandomDeletingDelay());
+  }else{
+    currentValue = thingToType.slice(0,currentValue.length+1)
+    setTimeout(update, getRandomTypingDelay());
+  }
+  domNode.innerText = currentValue;
+}
+
+function getRandomDeletingDelay() {
+  var min = 50, max = 100;
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomTypingDelay() {
+  var min = 50, max = 400;
+  return Math.random() * (max - min) + min;
+}
+
+update()
+
