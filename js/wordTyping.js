254506778907296df82ed891d3598223f37e30fd
jrob8577 <roberts.john@gmail.com>
2016-07-15 09:40:44 -0700

    Refactored typing

--- js/wordTyping.js
+++ js/wordTyping.js
@@ -1,49 +1,40 @@
+var PAUSE_DELAY = 500;
+var DELETE_DELAY = { min: 50, max: 100 };
+var TYPE_DELAY = { min: 50, max: 400 };
 
-var thingsToType = [
-  ' ONSITE COMPUTER SUPPORT'
-]
-
-var domNode = document.querySelector('.tagline');
-var pauseDelay = 500;
-
-var thingToTypeIndex = 0;
+var domNode = document.querySelector( '.tagline' );
 var currentValue = '';
 var deleting = false;
 
-function update(){
-  var thingToType = thingsToType[thingToTypeIndex];
-  if (!deleting && currentValue.length === thingToType.length){
+function update() {
+  var thingToType = 'ONSITE COMPUTER SUPPORT'
+
+  if ( !deleting && currentValue.length === thingToType.length ) {
     deleting = true;
-    setTimeout(update, pauseDelay);
+    setTimeout(update, PAUSE_DELAY);
     return;
   }
-  if (deleting && currentValue === ""){
+
+  if ( deleting && currentValue === "" ) {
     deleting = false;
-    thingToTypeIndex++
-    if (thingToTypeIndex >= thingsToType.length){
-      thingToTypeIndex = 0;
-    }
-    setTimeout(update, pauseDelay);
+
+    setTimeout( update, PAUSE_DELAY );
     return;
   }
-  if (deleting){
+
+  if ( deleting ) {
     currentValue = currentValue.slice(0,-1)
-    setTimeout(update, getRandomDeletingDelay());
-  }else{
+    setTimeout( update, delay( DELETE_DELAY ));
+  } else {
     currentValue = thingToType.slice(0,currentValue.length+1)
-    setTimeout(update, getRandomTypingDelay());
+    setTimeout( update, delay( TYPE_DELAY ));
   }
+
   domNode.innerText = currentValue;
 }
 
-function getRandomDeletingDelay() {
-  var min = 50, max = 100;
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomTypingDelay() {
-  var min = 50, max = 400;
-  return Math.random() * (max - min) + min;
+function delay( delayType ) {
+  return Math.random() * ( delayType.max - delayType.min ) + delayType.min;
 }
 
 update()
